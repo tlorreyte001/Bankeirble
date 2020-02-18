054e1c9db8da4cdd67810efe2098eb15464a0c36
Thomas Lorreyte <tom.lorreyte@gmail.com>
2020-02-18 21:27:40 +0100

    package.json fix

--- client/hello.js
+++ client/hello.js
@@ -1,3 +1,24 @@
+
+// import drizzle functions and contract artifact
+import { Drizzle } from "@drizzle/store";
+import MyStringStore from "./contracts/MyStringStore.json";
+
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [MyStringStore],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:7545",
+    },
+  },
+};
+
+// setup drizzle
+const drizzle = new Drizzle(options);
+
+
+
 const e = React.createElement;
 
 class LikeButton extends React.Component {
