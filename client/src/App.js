4c26fe42e5e8aeb17a8c7d45504b620e93f08630
amera.al.ktabe <43380936+aalktabe@users.noreply.github.com>
2020-03-17 11:32:10 +0100

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import { Login } from "./pages/Login.js";
-import { Signup } from "./pages/Signup.js";
+import { Login } from "./components/Login.js";
+import { Signup } from "./components/Signup.js";
 import { PrivateRoute } from "./components/PrivateRoute.js";
-import { Acceuil } from "./pages/Acceuil.js";
+import { Dashboard } from "./pages/Dashboard.js";
 import "./App.css";
+import './pages/styleAcceuil.css';
+import Welcome from './pages/Welcome';
+import Formulaire from './pages/Formulaire';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Switch>
-                    <Route exact path="/" component={Login} />
-                    <Route exact path="/sign-in" component={Login} />
-                    <Route exact path="/sign-up" component={Signup} />
-                    <PrivateRoute exact path="/acceuil" component={Acceuil} />
-                </Switch>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-content">
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/formulaire" component={Formulaire} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <PrivateRoute exact path="/acceuil" component={Acceuil} />         
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
